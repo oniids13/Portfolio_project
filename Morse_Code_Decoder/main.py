f9d6a582872066bf5b7a1307ba843049b89f6054
oniids13 <jdino.abaya@gmail.com>
2024-06-13 15:12:09 +0800

    3nd commit

--- Morse_Code_Decoder/main.py
+++ Morse_Code_Decoder/main.py
@@ -9,7 +9,15 @@ def morse_coding(text):
         else:
             print("Please enter valid character")
     print(f"Here's the morse code: \n{end_text}")
+    return end_text
 
+def decoding(dictionary, text):
+    decoded_text = []
+    for code in text:
+        for key, val in dictionary.items():
+            if val == code:
+                decoded_text.append(key)
+    print(f"The decoded value is {decoded_text}.")
 
 
 
@@ -18,8 +26,11 @@ while not should_end:
     print(logo)
     print("Welcome to my morse code decoder")
     text = input("Type the word to convert into morse code: \n")
-    morse_coding(text.upper())
+    decoded_text = morse_coding(text.upper())
 
+    decode = input("Do you want to decode your message? Type 'y' or 'n' \n")
+    if decode == "y":
+        decoding(morse, decoded_text)
     restart = input("Type 'yes' if you want to convert again, otherwise type 'no'.\n")
     if restart == "no":
         should_end = True
