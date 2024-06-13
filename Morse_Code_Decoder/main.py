83c6c2f613395e8583aab099029420d30b2498c3
oniids13 <jdino.abaya@gmail.com>
2024-06-13 14:44:58 +0800

    2nd commit

--- Morse_Code_Decoder/main.py
+++ Morse_Code_Decoder/main.py
@@ -1,5 +1,5 @@
-from morse_code import morse
-
+from morse_code import morse, logo
+import os
 def morse_coding(text):
     end_text = []
     for letter in text:
@@ -10,18 +10,20 @@ def morse_coding(text):
             print("Please enter valid character")
     print(f"Here's the morse code: \n{end_text}")
 
+
+
+
 should_end = False
 while not should_end:
+    print(logo)
+    print("Welcome to my morse code decoder")
     text = input("Type the word to convert into morse code: \n")
     morse_coding(text.upper())
 
-    restart = input("Type 'yes' if you want to code again, otherwise type 'no'.\n")
+    restart = input("Type 'yes' if you want to convert again, otherwise type 'no'.\n")
     if restart == "no":
-        should_end =  True
+        should_end = True
         print("Goodbye")
-
-
-# text = "DINO"
-# print(morse_coding(text))
+    os.system('cls')
 
 
