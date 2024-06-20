b0cd6ba6fb646fc73a694d06b3536dac7a49de2e
oniids13 <jdino.abaya@gmail.com>
2024-06-20 13:41:22 +0800

    first commit

---
+++ Breakout_game/main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(height=600, width=800)
+screen.title("Break out game")
+screen.tracer(0)
+
+
+
+
+screen.exitonclick()
