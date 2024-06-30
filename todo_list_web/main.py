2f9ff253653e837aea32028529678744561ca16e
oniids13 <jdino.abaya@gmail.com>
2024-06-30 17:48:18 +0800

    project done

--- todo_list_web/main.py
+++ todo_list_web/main.py
@@ -1,11 +1,5 @@
 from flask import Flask, abort, render_template, redirect, url_for, flash, request, get_flashed_messages
 from flask_bootstrap import Bootstrap5
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, BooleanField
-from wtforms.validators import DataRequired, URL
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Text, ForeignKey, Boolean
 from datetime import date
 
 
@@ -15,18 +9,22 @@ Bootstrap5(app)
 
 def read_tasks():
     tasks = []
+    done_tasks = []
     try:
         with open('tasks.txt', 'r') as file:
             for line in file:
                 description, done = line.strip().split('|')
-                tasks.append({'description': description, 'done': done == 'True'})
+                if done == 'True':
+                    done_tasks.append({'description': description, 'done': True})
+                else:
+                    tasks.append({'description': description, 'done': False})
     except FileNotFoundError:
         pass
-    return tasks
+    return tasks, done_tasks
 
-def write_tasks(tasks):
+def write_tasks(tasks, done_tasks):
     with open('tasks.txt', 'w') as file:
-        for task in tasks:
+        for task in tasks + done_tasks:
             file.write(f"{task['description']}|{task['done']}\n")
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -34,7 +32,7 @@ def home():
     today = today.strftime("%m/%d/%Y")
     year = date.today()
     year = year.year
-    tasks = read_tasks()
+    tasks, done_tasks = read_tasks()
     if request.method == 'POST':
         task = request.form.get('task')
         if task:
@@ -42,13 +40,19 @@ def home():
         elif 'toggle' in request.form:
             task_index = int(request.form.get('toggle'))
             tasks[task_index]['done'] = not tasks[task_index]['done']
+            if tasks[task_index]['done']:
+                done_tasks.append(tasks.pop(task_index))
         elif 'delete' in request.form:
             task_index = int(request.form.get('delete'))
-            tasks.pop(task_index)
+            section = request.form.get('section')
+            if section == 'tasks':
+                tasks.pop(task_index)
+            elif section == 'done_tasks':
+                done_tasks.pop(task_index)
 
-        write_tasks(tasks)
+        write_tasks(tasks, done_tasks)
         return redirect(url_for('home'))
-    return render_template('index.html', date=today, tasks=tasks, year=year)
+    return render_template('index.html', date=today, tasks=tasks, done_tasks=done_tasks, year=year)
 
 
 if __name__ == '__main__':
